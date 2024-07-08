81a55c8d0853b5bf3880e6cb6a7ebdd09e6b6fcf
hutchinsonvd <vandykehutch@yahoo.com>
2024-07-07 20:49:57 -0400

    lot of stuff

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import path from'path';
-import { getRandomPerson, getAllNationalities } from './postgres.js';
+import { getRandomPerson, getHardNationalities, getMediumNationalities, getEasyNationalities, getHelterNationalities } from './postgres.js';
 
 const __dirname = path.resolve(path.dirname('')); 
 
@@ -9,20 +9,65 @@ const __dirname = path.resolve(path.dirname(''));
 const app = express();
 const port = process.env.PORT || 8080;
 
+const SECRET = process.env.SECRET;
+//const SECRET = "SECRET";
+
 
 app.use(cors())
+app.use(express.json()) 
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/front-end/index.html'));
-  });
+app.use(function (req, res, next) {
+
+  //Here you would check for the user being authenticated
+
+  //Unsure how you're actually checking this, so some psuedo code below
+  if (SECRET != req.headers["authorization"]) {
+    res.sendStatus(401);
+
+    console.log("not authed");
+
+    return;
+  }
+  else {
+    //Carry on with the request chain
+    next();
+  }
+});
 
   app.get('/random', async function(req, res) {
+
     getRandomPerson()
     .then(response => res.send(response));
   });
 
-  app.get('/nationalities', async function(req, res) {
-    await getAllNationalities()
+  app.post('/nationalities/hard', async function(req, res) {
+  
+    await getHardNationalities(req.body.data)
+    .then(response => res.send(response));
+  });
+
+  app.post('/nationalities/medium', async function(req, res) {
+
+    await getMediumNationalities(req.body.data)
+    .then(response => res.send(response));
+  });
+
+  app.post('/nationalities/easy', async function(req, res) {
+    
+    await getEasyNationalities(req.body.data)
+    .then(response => res.send(response));
+  });
+
+  app.post('/nationalities/helterSkelter', async function(req, res) {
+
+    await getHelterNationalities(req.body.data)
+    .then(response => res.send(response));
+  });
+
+  app.post('/helterSkelter', async function(req, res) {
+
+    console.log(req.body)
+    await getHelterNationalities(req.body.data, req.body.score)
     .then(response => res.send(response));
   });
 
--- postgres.js
+++ postgres.js
@@ -7,6 +7,7 @@ const { Client } = pg
 const client = new Client({
     connectionString: process.env.PGCONNECTIONSTRING,
 ssl:false})
+//ssl:true})
 
     try {
         await client.connect()
@@ -26,7 +27,77 @@ export async function getAllNationalities() {
     .then(result => result.rows.map(row => row.nationality))
 }
 
-export async function getRandomNationalities() {
-    return await client.query("SELECT * FROM nationalities OFFSET floor(random() * 405) LIMIT 30;")
-    .then(result => result.rows.map(row => row.nationality))
+export async function getHardNationalities(person) {
+    return getNNationalities(person, 29);
+}
+
+async function getNNationalities(person, numResults) {
+
+    var nat = JSON.parse(person).nationality;
+    
+    return await getAllNationalities()
+    .then(nats => {
+        var randomIndices = [];
+
+        for (var i = 0; i < numResults; i++) {
+            var rand = Math.floor(Math.random() * (nats.length-1));
+    
+            randomIndices.push(rand);
+        }
+
+        var solutionIndex = Math.floor(Math.random() * (numResults));
+        if (numResults == 1) {
+
+            var results = [];
+            var a = Math.floor(Math.random() * 10);
+
+            if (a <= 5) {
+                results.push(nats.at(randomIndices.at(0)));
+                results.push(nat);
+            }
+            else {
+
+                results.push(nat);
+                results.push(nats.at(randomIndices.at(0)));        
+            }
+
+            return results;
+        }
+
+        var results = [];
+        
+        for (var i = 0; i < numResults; i++) {
+
+            if (solutionIndex == i) {
+                results.push(nat);
+            }
+
+            results.push(nats.at(randomIndices.at(i)));
+        }
+
+        return results;
+    })
+}
+
+export async function getMediumNationalities(person) {
+
+    return getNNationalities(person, 9);
+}
+
+export async function getEasyNationalities(person) {
+
+    return getNNationalities(person, 4);
+}
+
+export async function getHelterNationalities(person, score) {
+
+    var numToReturn = Math.pow(2, score);
+
+    if (numToReturn > 9) {
+        numToReturn = 9;
+    }
+    console.log(score);
+    console.log(numToReturn);
+
+    return getNNationalities(person, numToReturn);
 }
